71a1dc05576d1483d293b9f51dc0a25bdd51a074
Paweł Adamczak <pawel.ad@gmail.com>
2023-03-18 14:53:24 +0100

    Add `outputs.tf`

---
+++ tf/outputs.tf
@@ -0,0 +1,9 @@
+output "nimbus_ip_address" {
+  value       = digitalocean_droplet.nimbus.ipv4_address
+  description = "Nimbus Droplet public IP address."
+}
+
+output "pawelad_me_zone_id" {
+  value       = cloudflare_zone.pawelad_me.id
+  description = "Cloudflare 'pawelad.me' zone ID."
+}
