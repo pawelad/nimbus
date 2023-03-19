6c479f95dd9ee4441d3b866e2f22084918c63387
Paweł Adamczak <pawel.ad@gmail.com>
2023-03-19 16:29:16 +0100

    Add `pawelad_dev_zone_id` Terraform output

--- src/outputs.tf
+++ src/outputs.tf
@@ -7,3 +7,8 @@ output "pawelad_me_zone_id" {
   value       = cloudflare_zone.pawelad_me.id
   description = "Cloudflare 'pawelad.me' zone ID."
 }
+
+output "pawelad_dev_zone_id" {
+  value       = cloudflare_zone.pawelad_dev.id
+  description = "Cloudflare 'pawelad.dev' zone ID."
+}
