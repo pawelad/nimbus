2c3527882349eccf9ca2d6557435652ad55c50a7
Paweł Adamczak <pawel.ad@gmail.com>
2024-01-29 17:35:52 +0100

    Migrate to Spacelift, part III

--- src/spacelift.tf
+++ src/spacelift.tf
@@ -6,17 +6,3 @@ resource "spacelift_stack" "nimbus" {
   repository        = "nimbus"
   terraform_version = "1.5.7"
 }
-
-resource "spacelift_environment_variable" "nimbus_tf_var_droplet_username" {
-  stack_id   = spacelift_stack.nimbus.id
-  name       = "TF_VAR_droplet_username"
-  value      = "pawelad"
-  write_only = false
-}
-
-resource "spacelift_environment_variable" "nimbus_tf_var_dokku_domain" {
-  stack_id   = spacelift_stack.nimbus.id
-  name       = "TF_VAR_dokku_domain"
-  value      = "pawelad.me"
-  write_only = false
-}
