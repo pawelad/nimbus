2b74a94f605d26eed2ccf66361c27b23dcef3cf1
Paweł Adamczak <pawel.ad@gmail.com>
2024-01-29 17:39:23 +0100

    Migrate to Spacelift, part IV

--- src/spacelift.tf
+++ src/spacelift.tf
@@ -5,4 +5,9 @@ resource "spacelift_stack" "nimbus" {
   project_root      = "src"
   repository        = "nimbus"
   terraform_version = "1.5.7"
+
+  # 8< --------------------------------------------------------------
+  # Delete the following line after the stack has been created
+  import_state_file = "/mnt/workspace/state-import/nimbus.tfstate"
+  # -------------------------------------------------------------- 8<
 }
